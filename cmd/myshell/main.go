cc3226ed1600d749712d2a18752716acfc30f520
Punam <punamkumari399@gmail.com>
2025-01-04 21:20:40 +0530

    implemented REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,11 +14,20 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil{
+	for {
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	
+		if err != nil{
 		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		continue
+		}
+		command = command[:len(command)-1]
+		if command == "exit"{
+			break
+		}
+		fmt.Println(command + ": command not found")
+		fmt.Print("$ ")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
+	
 	
 }
